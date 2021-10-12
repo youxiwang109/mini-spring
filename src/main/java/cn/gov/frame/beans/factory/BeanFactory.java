8b448c08980c15e1d341d684b48bc7b2230a1cd2
中华-胡金生 <hujinsheng005@cic.cn>
2021-10-12 15:55:22 +0800

    init

---
+++ src/main/java/cn/gov/frame/beans/factory/BeanFactory.java
@@ -0,0 +1,13 @@
+package cn.gov.frame.beans.factory;
+
+/**
+ * @Description:
+ * @Author: hujinsheng
+ * @CreateDate: 2021/10/12 3:46 下午
+ * @Email: hujinsheng005@cic.cn
+ */
+public interface BeanFactory {
+
+
+   Object getBean(String name);
+}
